c22b2238bed93422faf321c6ea9b9056e1ff1b9d
oleeriksen <oe@easv.dk>
2021-02-15 22:16:58 +0100

    Initial commit

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Async Exercise - type 'q' to quit");
+
+            HttpClient _client = new HttpClient();
+
+            Console.Write(">");
+            char input = Console.ReadKey().KeyChar;
+            while (input != 'q')
+            {
+                DateTime start = DateTime.Now;
+
+                Task<HttpResponseMessage> response = _client.GetAsync("https://localhost:5003/numbers/12");
+
+                String result = (response.Result.Content.ReadAsStringAsync().Result);
+
+                WriteToFile("Result: " + result + " between " + start.ToLongTimeString() + " - " + DateTime.Now.ToLongTimeString());
+
+                Console.Write(">");
+                input = Console.ReadKey().KeyChar;
+            }
+        }
+
+        private static void WriteToFile(string s)
+        {
+            string path = @"fromclient.txt";
+
+            StreamWriter sw = File.AppendText(path);
+
+            sw.WriteLine(s);
+
+            sw.Flush();
+        }
+    }
+}
---
+++ ServerAPI/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ServerAPI.Controllers
+{
+    [ApiController]
+    [Route("numbers")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("{count}")]
+        public IEnumerable<int> GetPrimes(int count)
+        {
+            IEnumerable<int> result = GetEvenNumbersByAmount(count);
+            Thread.Sleep(2000);
+            return result;
+        }
+
+        private IEnumerable<int> GetEvenNumbersByAmount(int amountNeeded)
+        {
+            List<int> result = new List<int>();
+            int c = 0;
+            while (result.Count < amountNeeded)
+            {
+                result.Add(c);
+                c += 2;
+            }
+            return result;
+        }
+
+    }
+}
